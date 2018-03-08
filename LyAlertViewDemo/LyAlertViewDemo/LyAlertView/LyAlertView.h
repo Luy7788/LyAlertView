4bef55db07402fdd05f753c37e207df551643a54
Lying <lying_555@163.com>
2018-03-08 18:09:08 +0800

    注释

--- LyAlertViewDemo/LyAlertViewDemo/LyAlertView/LyAlertView.h
+++ LyAlertViewDemo/LyAlertViewDemo/LyAlertView/LyAlertView.h
@@ -17,12 +17,28 @@ typedef void(^ClickActionBlock)(NSInteger ButtonTag,NSString *ButtonTitle);
 @property (strong, nonatomic) UILabel           *messageLabel;
 @property (strong, nonatomic) NSMutableArray    *buttonArray;
 
+/**
+ 初始化方法
+
+ @param title 标题
+ @param message 描述信息
+ @param block 回调方法,返回按键的index和按键的titile
+ @param buttonTitles 传进按键的各个标题
+ @return instancetype
+ */
 -(instancetype)initWithTitile:(NSString *)title
                       Message:(NSString *)message
                    ClickBlock:(ClickActionBlock)block
                  ButtonTitles:(NSString *)buttonTitles, ... NS_REQUIRES_NIL_TERMINATION;
 
+/**
+ 显示
+ */
 -(void)show;
+
+/**
+ 隐藏
+ */
 -(void)dismiss;
 
 
